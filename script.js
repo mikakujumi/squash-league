55b8ed2a87edb7668836a50bc1bee1e4bcc0a11a
mikakujumi <kszajna@gmail.com>
2025-02-04 20:52:24 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Squash Liga Ergebnisse</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Ergebnistabelle</h1>
+    <table id="results-table">
+        <thead>
+            <tr>
+                <th>Platz</th>
+                <th>Vorname</th>
+                <th>Nachname</th>
+                <th>Punkte</th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+    </table>
+    <script src="script.js"></script>
+</body>
+</html>
